a4f5a7165385bdddac39c880a075c691b1db6f2d
김승환 <qaws5656@naver.com>
2021-03-06 00:17:40 +0900

    asdasdasd

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.use("/api/wargame", wargame_info);
 app.use("/api/admin", admin);
 app.use("/api/rank", rank);
 app.use("/api/LectureFiledownload", LectureFiledownload);
+//testasdads
 /* ROUTERS */
 
 app.get("/", (req, res) => {
